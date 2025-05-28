9f3e75160f08342c2c15a0b60d6657814b6ebca1
CornerstoneAndCompass <125543528+CornerstoneAndCompass@users.noreply.github.com>
2025-05-29 09:03:12 +1000

    1.9.8

--- streamlit_app.py
+++ streamlit_app.py
@@ -120,7 +120,7 @@ col3.metric("Gross Profit", f"${(filtered_data['Revenue'] - filtered_data['Payro
 
 st.subheader("📘 School by Number of Students by Term / Year")
 school_term_year = filtered_data.groupby(['School', 'Term', 'Year'])['Student Name'].nunique().reset_index(name='Student Count')
-st.dataframe(school_term_year, )
+st.dataframe(\1, use_container_width=True)
 
 st.subheader("📘 School by Instrument by Student Numbers")
 school_instrument = filtered_data.groupby(['School', 'Instrument'])['Student Name'].nunique().reset_index(name='Student Count')
